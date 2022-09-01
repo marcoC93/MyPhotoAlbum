fe29b2addec1aabc1b33a1f7472c34a9e5bda04b
marcoC93 <marco.chianese93@gmail.com>
2022-09-01 12:22:27 +0200

    Implementazione api lista foto ( singola e lista completa)

--- src/main/java/com/example/demo/controller/api/RestController.java
+++ src/main/java/com/example/demo/controller/api/RestController.java
@@ -2,8 +2,12 @@ package com.example.demo.controller.api;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.example.demo.model.Photo;
 
@@ -16,6 +20,26 @@ public class RestController {
 		
 		List<Photo> list = new ArrayList<Photo>();
 		list.add(new Photo(1, "/img/22.png"));
+		list.add(new Photo(2, "/img/23.png"));
+		list.add(new Photo(3, "/img/24.png"));
+		list.add(new Photo(4, "/img/25.png"));
 		return list ;
+	}
+	
+	@RequestMapping("api/photos/{id}")
+	public Photo getById(@PathVariable int id) {
+		List<Photo> list = new ArrayList<Photo>();
+		list.add(new Photo(1, "/img/22.png"));
+		list.add(new Photo(2, "/img/23.png"));
+		list.add(new Photo(3, "/img/24.png"));
+		list.add(new Photo(4, "/img/25.png"));
+		
+		Optional<Photo> photo = list.stream().filter(item->item.getId() == id).findFirst();
+		
+		if(photo.isEmpty()) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "item not found");
+		}
+		
+		return photo.get();
 	}
 }
