3754cac3ccca0a517145ce8e7970cf061d6d476c
marcoC93 <marco.chianese93@gmail.com>
2022-09-01 10:13:55 +0200

    Setup iniziale

---
+++ src/main/java/com/example/demo/MyPhotosAlbumApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPhotosAlbumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPhotosAlbumApplication.class, args);
+	}
+
+}
